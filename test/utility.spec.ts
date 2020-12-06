b15640ebb15a1b27419e5711d2fc709974bf77bc
Duy Tran <duy@Duys-MacBook-Pro.local>
2020-12-06 18:15:27 +0100

    Add documentation

--- test/utility.spec.ts
+++ test/utility.spec.ts
@@ -3,6 +3,6 @@ import { formatDate } from "../resolvers/Query";
 describe("formatDate", () => {
   it("should format the date", () => {
     const d = new Date("01/01/2011");
-    expect(formatDate(d)).toBe("02/01/2011");
+    expect(formatDate(d)).toBe("1/1/2011");
   });
 });
