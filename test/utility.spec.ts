74ab6a80f93d02bab3e51a4a9ca8f099e5f06d55
frallan97 <franssjos@gmail.com>
2020-12-07 16:36:41 +0100

    added get growt rate

--- test/utility.spec.ts
+++ test/utility.spec.ts
@@ -1,8 +1,23 @@
-import { formatDate } from "../resolvers/Query";
+import { formatDate, getGrowthRate } from "../resolvers/Query";
 
 describe("formatDate", () => {
   it("should format the date", () => {
     const d = new Date("01/01/2011");
     expect(formatDate(d)).toBe("1/1/2011");
   });
+});
+
+describe('getGrowtRate', () => {
+  it('should return 0 if index === 0', () => {
+    expect(getGrowthRate(0,[4,1,2,5])).toBe(0)
+  });
+
+  it('should return undefined if result[index-1] === 0 ', () => {
+    expect(getGrowthRate(1,[0,1,4,1,6])).toBe(undefined)
+  });
+
+  it('should return the growth rate from the following day', () => {
+    expect(getGrowthRate(1,[4,1,2,5])).toBe(3.5)
+  });
+  
 });
