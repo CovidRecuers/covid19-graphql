12f5583879fb5c01b72f43f6021f1eef842e903d
Duy Tran <duy@Duys-MacBook-Pro.local>
2020-12-08 15:06:21 +0100

    add test cases for result resolvers

---
+++ test/resolver.results.spec.ts
@@ -0,0 +1,31 @@
+import { ApolloError } from "apollo-server-micro";
+import { resolvers } from "../resolvers/Query";
+import * as multipleValueRes from "./mock-data/country/multiple-value.json";
+import * as singleValueRes from "./mock-data/country/single-value.json";
+import * as allData from "./mock-data/timeseries.json";
+
+describe("Result resolver", () => {
+  const mockContext = jest.fn();
+  it("should raise error with message 'Couldn't find data from country' if no data is provided for given country", async () => {
+    mockContext.mockReturnValueOnce(singleValueRes);
+    const t = async () =>
+      await resolvers.country(
+        null,
+        { name: "Denmark" },
+        { getResults: mockContext }
+      );
+
+    await expect(t()).rejects.toThrow(
+      "Couldn't find data from country Denmark"
+    );
+  });
+
+  it("should raise error if the date is invalid", () => {});
+
+  it("should return the most recent result if the date is not provided", () => {});
+
+  it("should return the correct result given countries and a date", () => {});
+
+  it("should return the correct results given countries and a minimum date", () => {});
+  it("should return the correct results given countries and a maximum date", () => {});
+});
